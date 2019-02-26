f57b3c79076069c6c30f4d223b53224725e91763
Osmel Guarepo <osmelg@hotmail.es>
2019-02-26 18:34:58 -0400

    b

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,34 +4,36 @@ import router from './router'
 import VeeValidate from 'vee-validate';
 import { Validator } from 'vee-validate';
 import VueSweetalert2 from 'vue-sweetalert2';
+import store from './store'
 
 Vue.config.productionTip = false
 // Middlewares
-  Vue.use(VueSweetalert2);
-  Vue.use(VeeValidate);
+Vue.use(VueSweetalert2);
+Vue.use(VeeValidate);
 // Validaciones VeeValidate
-  const dictionary = {
-    en:{
-      custom: {
-        nombre: {
-          required:'Type a name'
-        },
-        email: {
-          email:'Type a valid email',
-          required:'Type a email',
-        },
-        password: {
-          required:'Type a password',
-          alpha_num:'Password min 2 digits'
-        }
-      }    
-    }
-  };
-  Validator.localize(dictionary);
-  const validator = new Validator();
-  validator.localize('en'); 
+const dictionary = {
+  en:{
+    custom: {
+      nombre: {
+        required:'Type a name'
+      },
+      email: {
+        email:'Type a valid email',
+        required:'Type a email',
+      },
+      password: {
+        required:'Type a password',
+        alpha_num:'Password min 2 digits'
+      }
+    }    
+  }
+};
+Validator.localize(dictionary);
+const validator = new Validator();
+validator.localize('en');
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
